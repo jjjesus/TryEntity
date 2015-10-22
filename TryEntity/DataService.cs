41930249317af9b57caf394a2b900056a2ae9fc4
John Jesus <john.jesus@raytheon.com>
2015-10-22 12:55:24 -0500

    Provide sample output in comments.

--- TryEntity/DataService.cs
+++ TryEntity/DataService.cs
@@ -13,24 +13,39 @@ namespace TryEntity
             // Database context
             //
             try_entityEntities context = new try_entityEntities();
-            var mealGrouping = from meal in context.meals
+            var mealCourseGrouping = from meal in context.meals
                                group meal by new {  MealName = meal.meal_name,
                                                     CourseName = meal.course_name }
                                into mealCourse
                                orderby mealCourse.Key.MealName
                                select mealCourse;
-            if (mealGrouping != null)
+            if (mealCourseGrouping != null)
             {
-                foreach (var meal in mealGrouping)
+                foreach (var meal in mealCourseGrouping)
                 {
-                    var tuple = string.Format("Meal:{0} Course:{1}", meal.Key.MealName, meal.Key.CourseName);
-                    Console.WriteLine(tuple);
                     foreach (var course in meal)
                     {
-                        Console.WriteLine("  {0}", course.item);
+                        printMealCourseItem(course.meal_name, course.course_name, course.item);
                     }
                 }
             }
+        }
+
+
+        /* Output is
+            Meal:southern_dinner Course:appetizer Item:pickles
+            Meal:southern_dinner Course:dessert Item:pie
+            Meal:southern_dinner Course:main Item:steak
+            Meal:southern_dinner Course:salad Item:spinach
+            Meal:veggie_lunch Course:appetizer Item:carrots
+            Meal:veggie_lunch Course:dessert Item:strawberries
+            Meal:veggie_lunch Course:main Item:tofu burrito
+            Meal:veggie_lunch Course:salad Item:fruit
+         */
+        static void printMealCourseItem(string meal, string course, string item)
+        {
+            var tuple = string.Format("Meal:{0} Course:{1} Item:{2}", meal, course, item);
+            Console.WriteLine(tuple);
         }
     }
 }
